32f2938f3c2aad551a2c549fadedcd8223058884
andrew.brock <Andrew.Brock@sahmri.com>
2021-07-01 16:23:00 +0930

    Define Java min/max memory to align with XNAT recommendations
    Closes #8

--- packer_files/setenv.sh
+++ packer_files/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="${CATALINA_OPTS} -Dxnat.home=${XNAT_HOME}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dxnat.home=${XNAT_HOME} -Xms512m -Xmx4g -XX:+UseConcMarkSweepGC -XX:-OmitStackTraceInFastThrow"
